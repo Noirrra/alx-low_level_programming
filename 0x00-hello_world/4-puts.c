506132183f61de160c558b10c2b36013a9b51ff8
Noirrra <emmanuelfaith860@gmail.com>
2023-02-17 00:41:02 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - A programme that prints a line using puts function
+ *Return:0(Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle\n");
+	return (0);
+}
